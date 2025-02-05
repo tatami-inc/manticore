7c64f1e24c2054a59f9bcb927919ae72823f3974
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-02-05 18:48:04 +0000

    Deploying to gh-pages from @ tatami-inc/manticore@9cbf5531cbb1591441deb903d1ff2806626e1a54 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['executor_0',['executor',['../classmanticore_1_1Executor.html',1,'manticore::Executor'],['../index.html',1,'Main thread function executor']]],
-  ['executor_20tt_1',['Create a global &lt;tt&gt;Executor&lt;/tt&gt;',['../index.html#autotoc_md3',1,'']]]
+  ['executor_0',['Executor',['../classmanticore_1_1Executor.html',1,'manticore']]],
+  ['executor_1',['Main thread function executor',['../index.html',1,'']]],
+  ['executor_20tt_2',['Create a global &lt;tt&gt;Executor&lt;/tt&gt;',['../index.html#autotoc_md3',1,'']]]
 ];
--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['thread_20function_20executor_0',['Main thread function executor',['../index.html',1,'']]],
-  ['tt_20executor_20tt_1',['Create a global &lt;tt&gt;Executor&lt;/tt&gt;',['../index.html#autotoc_md3',1,'']]],
-  ['tt_20fetchcontent_20tt_2',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]],
-  ['tt_20find_5fpackage_20tt_3',['CMake using &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]]
+  ['tt_1',['tt',['../index.html#autotoc_md6',1,'CMake using &lt;tt&gt;find_package()&lt;/tt&gt;'],['../index.html#autotoc_md5',1,'CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;'],['../index.html#autotoc_md3',1,'Create a global &lt;tt&gt;Executor&lt;/tt&gt;']]],
+  ['tt_20executor_20tt_2',['Create a global &lt;tt&gt;Executor&lt;/tt&gt;',['../index.html#autotoc_md3',1,'']]],
+  ['tt_20fetchcontent_20tt_3',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]],
+  ['tt_20find_5fpackage_20tt_4',['CMake using &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]]
 ];
